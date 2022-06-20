6e42e40e617622e16eb0df7f1b6f09f72524cfdd
erdincdegirmenci <erdinc.degirmenci@teamdefinex.com>
2022-06-20 09:53:22 +0300

    arr

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -22,42 +22,40 @@
         cols="12"
       >
       <v-form
-    ref="form"
-    v-model="valid"
-    lazy-validation
-  >
-    <v-text-field
-      v-model="task"
-      :counter="10"
-      :rules="todoRules"
-      label="Task"
-      required
-    ></v-text-field>
+        ref="form"
+        v-model="valid"
+        lazy-validation
+      >
+        <v-text-field
+          v-model="task"
+          :counter="10"
+          :rules="todoRules"
+          label="Task"
+          required
+        ></v-text-field>
 
-   
-    <v-btn
-      :disabled="!valid"
-      color="success"
-      class="mr-4"
-      @click="addTodo"
-    >
-      Add
-    </v-btn>
+      
+        <v-btn
+          :disabled="!valid"
+          color="success"
+          class="mr-4"
+          @click="addTodo"
+        >
+          Add
+        </v-btn>
 
-    <v-btn
-      color="error"
-      class="mr-4"
-      @click="reset"
-    >
-      CLEAR
-    </v-btn>
-  </v-form>
+        <v-btn
+          color="error"
+          class="mr-4"
+          @click="reset"
+        >
+          CLEAR
+        </v-btn>
+      </v-form>
       </v-col>
-
-
     </v-row>
   </v-container>
-    <v-container>
+  <v-container>
     <v-row class="text-center">
         <v-col
         class="mb-5"
@@ -108,57 +106,7 @@
       task: '',
       todoRules: [
         v => !!v || 'Field is required'
-      ],
-      ecosystem: [
-        {
-          text: 'vuetify-loader',
-          href: 'https://github.com/vuetifyjs/vuetify-loader',
-        },
-        {
-          text: 'github',
-          href: 'https://github.com/vuetifyjs/vuetify',
-        },
-        {
-          text: 'awesome-vuetify',
-          href: 'https://github.com/vuetifyjs/awesome-vuetify',
-        },
-      ],
-      importantLinks: [
-        {
-          text: 'Documentation',
-          href: 'https://vuetifyjs.com',
-        },
-        {
-          text: 'Chat',
-          href: 'https://community.vuetifyjs.com',
-        },
-        {
-          text: 'Made with Vuetify',
-          href: 'https://madewithvuejs.com/vuetify',
-        },
-        {
-          text: 'Twitter',
-          href: 'https://twitter.com/vuetifyjs',
-        },
-        {
-          text: 'Articles',
-          href: 'https://medium.com/vuetify',
-        },
-      ],
-      whatsNext: [
-        {
-          text: 'Explore components',
-          href: 'https://vuetifyjs.com/components/api-explorer',
-        },
-        {
-          text: 'Select a layout',
-          href: 'https://vuetifyjs.com/getting-started/pre-made-layouts',
-        },
-        {
-          text: 'Frequently Asked Questions',
-          href: 'https://vuetifyjs.com/getting-started/frequently-asked-questions',
-        },
-      ],
+      ]
     }),
     methods: {
       reset () {
